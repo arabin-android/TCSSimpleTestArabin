4b555b640efe84db5434ca5c11974da3033baa8d
MISHU\14702 <arabin.tappware@gmail.com>
2022-12-28 10:05:06 -0500

    Desc<updated>

--- app/src/main/java/com/arabin/tcssimpletestarabin/ui/viewmodel/TeamsDataViewModel.kt
+++ app/src/main/java/com/arabin/tcssimpletestarabin/ui/viewmodel/TeamsDataViewModel.kt
@@ -15,28 +15,27 @@ import javax.inject.Inject
 
 /**
  * @author Arabin
- * @since 12/16/2022
+ * @since 12/28/2022
  * Main view model to communicate with Main Repo
- * calls db queries and API's
  * */
 @HiltViewModel
 class TeamsDataViewModel @Inject constructor(private val mainRepo: MainRepo) : ViewModel() {
 
-    /**live school list data to be observed in UI*/
+    /**live teams list data to be observed in UI*/
     private val _localSchoolData = MutableLiveData<Event<RestAPIState>>()
     var localSchoolData: LiveData<Event<RestAPIState>> =
         _localSchoolData
 
 
     init {
-        loadSchoolDetails()
+        loadTeamsDetails()
     }
 
 
-    /** 1st check if data already populated in roomDb or not
-     * if not fetch api and insert it to roomdb
-     * runs in background thread both operations api and room insertion*/
-    fun loadSchoolDetails() {
+    /**
+     * call api in IO thread to get data
+     * */
+    fun loadTeamsDetails() {
         viewModelScope.launch (Dispatchers.IO){
             _localSchoolData.postValue(Event(RestAPIState.Loading(null, "Loading")))
             try {
